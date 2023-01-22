f156366de7cae00f3e14c4806a2b1c2fad105857
brj <bjensen@fastmail.fm>
2023-01-22 18:47:46 +0100

    added tests verifying that blank lines and new lines are handled correctly

--- src/test/java/dk/grixie/jinja2/engine/TemplateEngineTest.java
+++ src/test/java/dk/grixie/jinja2/engine/TemplateEngineTest.java
@@ -20,7 +20,7 @@ public class TemplateEngineTest {
 
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
 
-        engine.generate(new InputStreamReader(TemplateEngineTest.class.getResourceAsStream("/template.j2")),
+        engine.generate(new InputStreamReader(TemplateEngineTest.class.getResourceAsStream("/single-line-template.j2")),
                 Map.of("to", "Alice", "from", "Bob"),
                 new OutputStreamWriter(byteArrayOutputStream)
         );
@@ -28,6 +28,45 @@ public class TemplateEngineTest {
         BufferedReader reader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(byteArrayOutputStream.toByteArray())));
 
         assertThat(reader.readLine()).isEqualTo("hello Alice from Bob.");
+        assertThat(reader.readLine()).isNull();
+    }
+
+    @Test
+    public void testGenerateTwoLineTemplate() throws IOException {
+        TemplateEngine engine = new TemplateEngine();
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+        engine.generate(new InputStreamReader(TemplateEngineTest.class.getResourceAsStream("/double-line-template.j2")),
+                Map.of("to", "Alice", "from", "Bob", "regards", "your friend"),
+                new OutputStreamWriter(byteArrayOutputStream)
+        );
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(byteArrayOutputStream.toByteArray())));
+
+        assertThat(reader.readLine()).isEqualTo("hello Alice from Bob.");
+        assertThat(reader.readLine()).isEqualTo("Best regards to your friend as well.");
+        assertThat(reader.readLine()).isNull();
+    }
+
+    @Test
+    public void testGenerateBlankLineTemplate() throws IOException {
+        TemplateEngine engine = new TemplateEngine();
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+        engine.generate(new InputStreamReader(TemplateEngineTest.class.getResourceAsStream("/blank-lines-template.j2")),
+                Map.of("to", "Alice", "from", "Bob", "regards", "your friend"),
+                new OutputStreamWriter(byteArrayOutputStream)
+        );
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(byteArrayOutputStream.toByteArray())));
+
+        assertThat(reader.readLine()).isEqualTo("");
+        assertThat(reader.readLine()).isEqualTo("hello Alice from Bob.");
+        assertThat(reader.readLine()).isEqualTo("");
+        assertThat(reader.readLine()).isEqualTo("Best regards to your friend as well.");
+        assertThat(reader.readLine()).isEqualTo("");
         assertThat(reader.readLine()).isNull();
     }
 }
